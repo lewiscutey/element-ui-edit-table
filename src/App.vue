2adde88b47c38325ece1dd96a5a627cffaa21fe0
lvxin <v_lvxin01@baidu.com>
2017-06-18 18:16:15 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="edit-table">
+        
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            
+        };
+    },
+    computed: {
+       
+    },
+    methods: {
+        
+    },
+    watch: {
+       
+    }
+};
+</script>
+<style>
+
+</style>
